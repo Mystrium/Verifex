94d9d04313cff2542ad02f5d3d15b977b0085393
Mystrium <godunokanton@gmail.com>
2024-03-27 10:33:02 +0200

    fix register get -> post

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/login',       [ApiController::class, 'login']);
-Route::get('/register',    [ApiController::class, 'register']);
+Route::post('/register',    [ApiController::class, 'register']);
 Route::get( '/cehs',        [ApiController::class, 'cehs']);
 Route::post('/roles',       [ApiController::class, 'roles']);
 Route::post('/items',       [ApiController::class, 'items']);
