b40cfda1e6e39491a5ba737232987387177acbe1
Mystrium <godunokanton@gmail.com>
2024-04-30 14:16:54 +0300

    selfcost accordion js, api operations history changed id

--- resources/views/cost.blade.php
+++ resources/views/cost.blade.php
@@ -10,10 +10,10 @@
                 <th scope="col">#</th>
                 <th scope="col">Назва</th>
                 <th scope="col">Фото</th>
-                <th scope="col">-</th>
-                <th scope="col"></th>
-                <th scope="col"></th>
-                <th scope="col"></th>
+                <th scope="col">Опис</th>
+                <th scope="col">За роботу</th>
+                <th scope="col">Кількість</th>
+                <th scope="col">Сума</th>
             </tr>
         </thead>
         <tbody>
@@ -30,51 +30,59 @@
                 <td>{{$item->unit}}</td>
                 <td>-</td>
             </tr>
-            <tr class="collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#a{{$item->id}}">
+            <tr type="button" onclick="hiderows('a{{$item->id}}')">
+                <th>Операції</th>
+                <th colspan=6 id="1a{{$item->id}}">[ + ]</th>
+            </tr>
+            <tr name="a{{$item->id}}" hidden>
                 <th scope="col">#</th>
-                <th scope="col">Операції</th>
-                <th scope="col">Опис</th>
+                <th scope="col">Назва</th>
                 <th scope="col">Фото</th>
+                <th scope="col">Опис</th>
                 <th scope="col">За роботу</th>
                 <th scope="col">Кількість</th>
                 <th scope="col">Сума</th>
             </tr>
             @php($total = 0)
             @foreach($item->work as $sub)
-                <tr id="a{{$item->id}}" class="accordion-collapse collapse accordion-body">
+                <tr name="a{{$item->id}}" hidden>
                     <td>{{ $loop->iteration }}</td>
                     <td>{{$sub->title}}</td>
-                    <td>{{$sub->description}}</td>
                     <td>
                         <a href="/items/edit/{{$sub->have_id}}">    
                             <img src="{{$sub->url_photo}}" style="max-width:100px; max-height:100px">
                         </a>
                     </td>
+                    <td>{{$sub->description}}</td>
                     <td>{{round($sub->price, 2)}}₴</td>
                     <td>{{$sub->count + 0}}</td>
                     <td>{{round($sub->count * $sub->price, 2)}}₴</td>
                 </tr>
                 @php($total += $sub->count * $sub->price)
             @endforeach
-            <tr class="collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#b{{$item->id}}">
+            <tr type="button" onclick="hiderows('b{{$item->id}}')">
+                <th>Матеріали</th>
+                <th colspan=6 id="1b{{$item->id}}">[ + ]</th>
+            </tr>
+            <tr name="b{{$item->id}}" hidden>
                 <th scope="col">#</th>
-                <th scope="col">Матеріали</th>
-                <th scope="col">Опис</th>
+                <th scope="col">Назва</th>
                 <th scope="col">Фото</th>
+                <th scope="col">Опис</th>
                 <th scope="col">Ціна</th>
                 <th scope="col">Кількість</th>
                 <th scope="col">Сума</th>
             </tr>
             @foreach($item->subitems as $sub)
-                <tr id="b{{$item->id}}" class="accordion-collapse collapse accordion-body">
+                <tr name="b{{$item->id}}" hidden>
                     <td>{{ $loop->iteration }}</td>
                     <td>{{$sub->title}}</td>
-                    <td>{{$sub->description}}</td>
                     <td>
                         <a href="/items/edit/{{$sub->have_id}}">    
                             <img src="{{$sub->url_photo}}" style="max-width:100px; max-height:100px">
                         </a>
                     </td>
+                    <td>{{$sub->description}}</td>
                     <td>{{$sub->price ? round($sub->price, 2) . '₴' : 'He закупленo'}}</td>
                     <td>{{$sub->cnt + 0}} {{$sub->unit}}</td>
                     <td>{{round($sub->cnt * $sub->price, 2)}}₴</td>
@@ -87,10 +95,27 @@
                 <td></td>
                 <td></td>
                 <td></td>
-                <td>Всього</td>
-                <td>{{round($total, 2)}}₴</td>
+                <td class="fw-bold">Всього</td>
+                <td class="fw-bold">{{round($total, 2)}}₴</td>
             </tr>
         </tbody>
     </table>
 @endforeach
+<script>
+    function hiderows(id) {
+        var a = document.getElementsByName(id);
+        var text = document.getElementById('1' + id)
+        if(a[0].hidden == false) {
+            a.forEach(element => {
+                element.hidden = true
+            });
+            text.innerHTML = "[ + ]"
+        } else {
+            a.forEach(element => {
+                element.hidden = false
+            });
+            text.innerHTML = "[ - ]"
+        }
+    }
+</script>
 @endsection
